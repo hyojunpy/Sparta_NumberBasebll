d3ac7989c0f5ceea30a0bae1d3070857f296ab96
Hyojun Pyeon <vusgywns5946@gmail.com>
2024-10-25 13:43:14 +0900

    chore : 정답 생성 부분 수정 및 ReadMe 추가

--- src/Lv1/BaseballGame.java
+++ src/Lv1/BaseballGame.java
@@ -4,10 +4,8 @@ import java.util.*;
 
 public class BaseballGame {
     Scanner sc = new Scanner(System.in);
-    // 게임 진행 횟수
-    private int count = 0;
     //게임을 진행할 숫자
-    List<Integer> Attack = new ArrayList<Integer>();
+    List<Integer> Attack = new ArrayList<>();
     //들어온 숫자
     String input;
     //난수 생성하기 위한 객체 생성
@@ -20,40 +18,51 @@ public class BaseballGame {
     int ball = 0;
     //난이도 조절
     int level = 3;
+    //게임 횟수
+    private static int gameCount = 0;
+    // 게임 진행 횟수
+    private static int count = 0;
+    //게임 횟수 별 진행 횟수 저장
+    static List<Integer> gameCounter = new ArrayList<>();
 
 
-    //객체 생성시 정답 출력
+    //객체 생성시 정답 생성
     public BaseballGame() {
-        play();
+        //난수 생성(중복없이 난이도에 맞는 갯수가 채워질때 까지 )
+        while (true) {
+            int randomNumber = random.nextInt(8) + 1;
+            if (!randomNum.contains(randomNumber)) {
+                randomNum.add(randomNumber);
+            }
+            if (randomNum.stream().distinct().count() == level) break;
+        }
     }
 
-    public int play() {
-            System.out.println("< 게임을 시작합니다 >");
+    public void play() {
+        count = 0;
+        gameCounter = new ArrayList<>();
+        System.out.println("< 게임을 시작합니다 >");
         while (true) {
             System.out.println("숫자를 입력하세요 : ");
             input = sc.nextLine();
+            // 게밍 진행 횟수 초기화
             if (validateInput(input)) {
                 //게임 진행 횟수 추가
                 count++;
-                //난수 생성(중복없이 난이도에 맞는 갯수가 채워질때 까지 )
-                while (randomNum.stream().distinct().count() < level) {
-                    int a = random.nextInt(8) + 1;
-                    randomNum.add(a);
-                    System.out.println(a);
-                }
                 //스트라이크,볼 개수 계산 다 맞췄을시 정답 출력 후 종료
                 countStrikeAndBall(randomNum, Attack);
 
                 if (strike == level) {
                     System.out.println("정답입니다 축하드립니다.");
                     break;
-                }else BaseballGameDisplay.displayHint(strike, ball);
-
+                } else BaseballGameDisplay.displayHint(strike, ball);
                 Attack.clear();
             }
         }
-        //게임진행 횟수 반환
-        return count;
+        //게임 횟수 추가
+        gameCount++;
+        //게임 진행 횟수 저장
+        gameCounter.add(count);
     }
 
     protected boolean validateInput(String input) {
@@ -62,15 +71,15 @@ public class BaseballGame {
             for (int i = 0; i < input.length(); i++) {
                 Attack.add((int) input.charAt(i) - '0');
                 //0이 들어왔을때 false
-                if(Attack.contains(0)){
+                if (Attack.contains(0)) {
                     System.out.println("올바르지 않은 입력값입니다.");
                     Attack.clear();
                     return false;
                 }
             }
-        }else System.out.println("올바르지 않은 입력값입니다.");
+        } else System.out.println("올바르지 않은 입력값입니다.");
 
-        //중복된 값 검사 : distinct로 중복요소 제거 => 난이도와 같을 시 true
+        //중복된 값 검사 : distinct()로 중복요소 제거 => 난이도와 같을 시 true
         if (Attack.stream().distinct().count() == level) {
             return true;
         }
@@ -88,13 +97,20 @@ public class BaseballGame {
         //스트라이크 볼 카운트
         for (int i = 0; i < random.size(); i++) {
             for (int j = 0; j < attackNum.size(); j++) {
-                if(random.get(i).equals(attackNum.get(j))) {
-                    if(i != j) {
+                if (random.get(i).equals(attackNum.get(j))) {
+                    if (i != j) {
                         ball++;
-                    }
-                    else strike++;
+                    } else strike++;
                 }
             }
         }
+    }
+
+    public static int getGameCount() {
+        return gameCount;
+    }
+
+    public static int getCount() {
+        return count;
     }
 }
--- src/Lv1/Main.java
+++ src/Lv1/Main.java
@@ -7,23 +7,32 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        BaseballGame baseballGame;
+        boolean switchs = true;
 
         System.out.println("환영합니다! 원하시는 번호를 입력해주세요");
 
-        while (true) {
+        while (switchs) {
             System.out.println("1.게임 시작하기 2. 게임 기록 보기 3. 종료하기");
+            BaseballGame baseballGame = new BaseballGame();
             String input = sc.nextLine();
             //1일시 게임 시작
-            if(input.equals("1") ) {
-                baseballGame = new BaseballGame();
+            switch (input) {
+                //1 입력시 게임 진행
+                case "1" -> baseballGame.play();
+                //2 입력시 게임 회차 및 게임 진행 횟수 출력
+                case "2" -> {
+                    for (int j = 1; j <= BaseballGame.getGameCount(); j++) {
+                        System.out.println(j + "번째 게임 : 시도 횟수 - " + BaseballGame.gameCounter.get(j -1));
+                    }
+                }
+                //3입력 시 종료
+                case "3" -> {System.out.println("< 숫자 야구 게임을 종료합니다 > "); switchs =false; }
+                //1,2,3 이외에 다른 입력값 입력시
+                default -> System.out.println("올바른 숫자를 입력해주세요!");
             }
-            else if(input.equals("2")) {
-                //Lv2 아직 기능 없음
-            } // 3일시 종료
-            else  if(input.equals("3")) break;
-            // 1,2,3이 아닐시 경고
-            else System.out.println("올바른 입력값이 아닙니다.");
+
         }
     }
+
+
 }
